7b3f542d85fa2385d0de5e34b7f809ee9edf3b35
Masora <masora@menhera.org>
2021-03-11 18:40:23 +0900

    updates

--- estoolkit/index.js
+++ estoolkit/index.js
@@ -2,8 +2,21 @@
 {
     // BEGIN PRELUDE
     const prelude = globalThis.prelude = Object.create(null);
+    Reflect.defineProperty(globalThis, 'prelude', {value: prelude});
     // END PRELUDE
 
+    prelude.TypedArray = Reflect.getPrototypeOf(Uint8Array);
+
+    prelude.getRandomArrayBuffer = (byteLength) => (new Uint8Array(function * () {
+        for (let i = 0; i < (0 | byteLength); i++) {
+            yield (Math.random() * 0x100) & 0xff;
+        }
+    }())).buffer;
+
+    prelude.arrayBufferToHex = (arrayBuffer) => Array.prototype.map.call(
+        new Uint8Array(arrayBuffer)
+        ,byte => (0x100 & byte).toString(0x10).slice(-2)
+    ).join('');
 
     /**
      * Returns true if and only if the given value has the [[Construct]] internal slot.
